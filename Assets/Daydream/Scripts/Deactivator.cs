f82ca26b1cb4a50c54ae9e2e7f9da577d14c8cd9
laurmaedje <laurmaedje@gmail.com>
2016-09-20 15:03:46 +0200

    add moving robot

---
+++ Assets/Daydream/Scripts/Deactivator.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Deactivator : MonoBehaviour {
+
+    public enum Action {
+        Destroy, 
+        Deactivate
+    }
+
+    [SerializeField] float delay;
+    [SerializeField] Action type;
+
+    float timer;
+
+	// Use this for initialization
+	void Start () {
+        timer = delay;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (timer > 0) {
+            timer -= Time.deltaTime;
+        }
+        else {
+            if (type == Action.Deactivate) {
+                timer = delay;
+                gameObject.SetActive(false);
+            } else {
+                Destroy(gameObject);
+            }
+
+        }
+    }
+}
--- Assets/Daydream/Scripts/Player.cs
+++ Assets/Daydream/Scripts/Player.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class Player : MonoBehaviour {
 
+    [SerializeField] GameObject clickEffect;
+
+    [SerializeField] float moveSpeed;
+    [SerializeField] float rotateSpeed;
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
@@ -14,8 +20,37 @@ public class Player : MonoBehaviour {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit)) {
-                transform.position = new Vector3(hit.point.x, 1f, hit.point.z);
+                StopCoroutine("MoveTo");
+                StartCoroutine("MoveTo", new Vector3(hit.point.x, 1f, hit.point.z));
+                clickEffect.transform.position = new Vector3(hit.point.x, 1f, hit.point.z);
+                clickEffect.transform.GetChild(0).GetComponent<ParticleSystem>().Play();
             }
         }
+
 	}
+
+    IEnumerator MoveTo(Vector3 target) {
+
+        // Rotate
+        float angle = Vector3.Angle(target - transform.position, transform.forward);
+        float sign = -Mathf.Sign(Vector3.Cross(target - transform.position, transform.forward).y); 
+        float rotation = 0f;
+        while(rotation < angle) {
+            transform.Rotate(0, sign * rotateSpeed * Time.deltaTime, 0f);
+            rotation += rotateSpeed * Time.deltaTime;
+            yield return null;
+        }
+
+        // Move
+        Vector3 dir = target - transform.position;
+        dir.y = 0f;
+        float distance = Vector3.Distance(target, transform.position);
+        float moved = 0f;
+        dir.Normalize();
+        while (moved < distance) {
+            transform.position += moveSpeed * dir * Time.deltaTime;
+            moved += moveSpeed * Time.deltaTime;
+            yield return null;
+        }   
+    }
 }
